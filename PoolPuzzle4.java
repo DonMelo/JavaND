23548bf0797b4cb2892b8abfb9a91c7fdaa86e26
DonMelo <36507553+DonMelo@users.noreply.github.com>
2020-11-17 18:45:33 +0200

    antradienio užduotys

---
+++ PoolPuzzle4.java
@@ -0,0 +1,35 @@
+package JavaND;
+
+public class PoolPuzzle4 {
+    public static void main(String[] args) {
+        PoolPuzzle4[] obs = new PoolPuzzle4[6];
+        int y = 1;
+        int x = 0;
+        int result = 0;
+        while (x < 6) {
+            obs[x].ivar = x;
+            obs[x].ivar = y;
+            y = y * 10;
+            x = x + 1;
+        }
+        x = 6;
+        while (x > 0) {
+            x = x - 1;
+            result = result + 
+        }
+        System.out.println("result " + result);
+    }}
+
+    class{
+
+    int ivar;
+
+    doStuff(int ) {
+        if (ivar > 100) {
+            return 
+        }
+        else {
+            return
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JavaND
+
+# Chapter 3- masyvai ne tokie baisūs kaip tikėtąsi. Patiko paprasta valdymo pultelio analogija
+# Chapter 4- . operatorius palengvina kodo skaitymą bei papildžiau žinias apie masyvus bei supratau skirtumus tarp public ir private pritaikymą praktikoje
